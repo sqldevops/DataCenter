a79118cf3658971051128062e3c5c02ae09ccdd8
ori <ori@crow.co.il>
2016-08-11 18:22:52 +0300

    Edit Deploy Scripts

--- DeployScripts/TestProj.ps1
+++
@@ -1,5 +0,0 @@
-﻿if (Test-Path $test_results_path) {Remove-Item $test_results_path};
-# Database unit tests supported only in old MSTest, not in VSTest.Console.Exe  .
-# Link: https://msdn.microsoft.com/en-us/library/ms253138(v=vs.110).aspx#Runner.
-# Cant run ordertest and playlist in MSTest, order is hopefully set as "alphnumeric".
-mstest /testcontainer:$test_path /category:$test_category /resultsfile:"$test_results_path";
